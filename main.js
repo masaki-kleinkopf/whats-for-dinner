019d7e1b57955118c6963d187248e76c8370a153
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-04-10 11:03:59 -0700

    Add animation edits and README gifs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //query selectors
-var cookButton = document.querySelector('.lets-cook')
-var showFoodArea = document.querySelector('.cookpot-container')
-var cookpot = document.querySelector('.cook-pot')
-var recipeContainer = document.querySelector('.recipe-container')
+var cookButton = document.querySelector('.lets-cook');
+var showFoodArea = document.querySelector('.cookpot-container');
+var cookpot = document.querySelector('.cook-pot');
+var recipeContainer = document.querySelector('.recipe-container');
 
 //DATA MODEL
 var sides = ['Miso Glazed Carrots',
@@ -48,67 +48,26 @@ var desserts = ['Apple Pie',
 'Eclairs'];
 
 //event listeners
-cookButton.addEventListener('click', randomFood )
+cookButton.addEventListener('click', randomFood);
 //event handlers
 
 function randomFood(){
   var allFoods ={sides:sides,
     mainDishes:mainDishes,
     desserts:desserts};
-
   selectedBtn = document.querySelector('input[name="radio-btn"]:checked').value;
-  cookpot.classList.add('cook-pot-animate')
-  //fadeOutEffect();
-  randomItem(allFoods[selectedBtn])
-  unfade(addedText);
-}
+  randomItem(allFoods[selectedBtn]);
+  animate();
+};
 
-function fadeOutEffect() {
-
-    var fadeEffect = setInterval(function () {
-        if (!cookpot.style.opacity) {
-            cookpot.style.opacity = 1;
-        }
-        if (cookpot.style.opacity > 0) {
-            cookpot.style.opacity -= 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 400);
-}
-
-
-function fadeInEffect() {
-    var addedText = document.querySelector('.added-text')
-    var fadeEffect = setInterval(function () {
-        if (!addedText.style.opacity) {
-            addedText.style.opacity = 0;
-        }
-        if (addedText.style.opacity < 1) {
-            addedText.style.opacity += 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 400);
-}
-var addedText = document.querySelector('div')
-
-function unfade(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 100);
-}
-
+function animate(){
+  cookpot.classList.remove('cook-pot-animate');
+  void cookpot.offsetWidth;
+  cookpot.classList.add('cook-pot-animate');
+};
 
 function randomItem(food){
-  var randomItem = food[Math.floor(Math.random()*food.length)]
-  recipeContainer.innerHTML= `<div class = "added-text"><h4> You should make </h4>
-                            <h2>${randomItem}</h2></div>`
-  }
+  var randomItem = food[Math.floor(Math.random()*food.length)];
+  recipeContainer.innerHTML= `<div class = "added-text faded-out"><h4> You should make </h4>
+                            <h2>${randomItem}</h2></div>`;
+};
